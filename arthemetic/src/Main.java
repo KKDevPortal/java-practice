83e3e2f90d22e677aa175c1360d34b1f49ff03a6
Kaushal Kishor <kaushal.kishor@niit.com>
2025-12-29 21:10:13 +0530

    added arithematic operation

--- arthemetic/src/Main.java
+++ arthemetic/src/Main.java
@@ -6,17 +6,76 @@ public class Main {
     static int sumOfTwoNumber(int x, int y) {
         return x+y;
     }
-    public static void main(String[] args) {
+
+    static  int substractTwoNumber(int x, int y) {
+        return x-y;
+    }
+
+    static int multiplication(int x, int y) {
+        return x*y;
+    }
+
+    static float division_withReinder(int x, int y) {
+        if(x < 0 || y <0) {
+            return 0;
+        } else {
+            return x%y;
+        }
+    }
+
+    static float division_quoient(int x, int y) {
+        if(x < 0 || y <0) {
+            return 0;
+        } else {
+            return x/y;
+        }
+    }
+
+    static void choice(int ch) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number_1: ");
-        int num1 = sc.nextInt();
+        int x = sc.nextInt();
 
         System.out.print("Enter a number_2: ");
-        int num2 = sc.nextInt();
+        int y = sc.nextInt();
 
-        System.out.println("You entered: \nnum1 " + num1 + "\nnum2 "+ num2 );
+        System.out.println("You entered: \nnum1 " + x + "\nnum2 "+ y );
 
-        System.out.println("Sub is :: " + sumOfTwoNumber(num1, num2));
+        switch(ch) {
+            case 1:
+                System.out.println(sumOfTwoNumber(x, y));
+                break;
+            case 2:
+                System.out.println(substractTwoNumber(x, y));
+                break;
+            case 3:
+                System.out.println(multiplication(x,y));
+                break;
+            case 4:
+                System.out.println(division_quoient(x,y));
+                break;
+            case 5:
+                System.out.println(division_withReinder(x,y));
+            default:
+                System.out.println("Thank you for using this application, please try again.....");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Calculation opeeration\n---------------------------------------------------\n");
+        System.out.println("Select for following operation :");
+        System.out.println("\n1. ADD TWO NUMBER");
+        System.out.println("\n2. SUBTRACT TWO NUMBER");
+        System.out.println("\n3. MULTIPLICATION OF TWO NUMBER");
+        System.out.println("\n4. DIVISION WITH QUOTIENT");
+        System.out.println("\n5. DIVISION WITH REMINDER");
+        System.out.println("\n---------------------------------------------------");
+
+        System.out.print("Select your choice:> ");
+        int choice = sc.nextInt();
+        choice(choice);
     }
 }
