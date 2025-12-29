29d6ebd91ac1c5b46a0c79378b2efc3ff2933264
Kaushal Kishor <kaushal.kishor@niit.com>
2025-12-29 22:01:06 +0530

    struct

--- hello-world/src/Main.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static <String> void main(String[] args) {
-        System.out.println("Hello Every one, this is my first program");
-    }
-}
